4f504e000988f8013dee4737e31e5aac478e829d
jeongEY <kingship89@naver.com>
2023-06-19 18:16:03 +0900

    2023/06 페이징

--- src/test/java/com/spring/jpa/chap01_basic/repository/ProductRepositoryTest.java
+++ src/test/java/com/spring/jpa/chap01_basic/repository/ProductRepositoryTest.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-import static com.spring.jpa.chap01_basic.entity.Product.*;
 import static com.spring.jpa.chap01_basic.entity.Product.Category.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,27 +34,23 @@ class ProductRepositoryTest {
         Product p2 = Product.builder()
                 .name("탕수육")
                 .category(FOOD)
-                .price(100000)
+                .price(20000)
                 .build();
         Product p3 = Product.builder()
                 .name("구두")
                 .category(FASHION)
-                .price(150000)
+                .price(300000)
                 .build();
         Product p4 = Product.builder()
                 .name("쓰레기")
                 .category(FOOD)
                 .build();
+
         //when
         Product saved1 = productRepository.save(p1);
         Product saved2 = productRepository.save(p2);
         Product saved3 = productRepository.save(p3);
         Product saved4 = productRepository.save(p4);
-
-
-        //then
-        assertNotNull(saved1);
-        assertNotNull(saved3);
     }
 
     @Test
@@ -63,7 +58,7 @@ class ProductRepositoryTest {
     void testSave() {
         Product product = Product.builder()
                 .name("정장")
-                .price(10000000)
+                .price(1000000)
                 .category(FASHION)
                 .build();
 
@@ -80,29 +75,31 @@ class ProductRepositoryTest {
         //when
         productRepository.deleteById(id);
         //then
-
     }
-
+    
     @Test
     @DisplayName("상품 전체조회를 하면 상품의 개수가 4개여야 한다.")
     void testFindAll() {
         //given
-
+        
         //when
-        List<Product> productList = productRepository.findAll();
+        List<Product> products = productRepository.findAll();
+
         //then
-        productList.forEach(System.out::println);
+        products.forEach(System.out::println);
 
-        assertEquals(4, productList.size());
+        assertEquals(4, products.size());
     }
-
+    
     @Test
     @DisplayName("3번 상품을 조회하면 상품명이 '구두'여야 한다.")
     void testFindOne() {
         //given
         long id = 3L;
         //when
-        Optional<Product> product = productRepository.findById(id);
+        Optional<Product> product
+                = productRepository.findById(id);
+
         //then
         product.ifPresent(p -> {
             assertEquals("구두", p.getName());
@@ -113,7 +110,6 @@ class ProductRepositoryTest {
 
     }
 
-
     @Test
     @DisplayName("2번 상품의 이름과 가격을 변경해야 한다.")
     void testModify() {
@@ -123,9 +119,9 @@ class ProductRepositoryTest {
         int newPrice = 6000;
 
         //when
-        //jpa에서 update는 따로 메서드를 지워하지 않고
-        //조회를 한 후 setter로 변경하면 자동으로 update문이 나간다.
-        //변경 후 다시 save를 호출.
+        //jpa에서 update는 따로 메서드를 지원하지 않고
+        //조회를 한 후 setter로 변경하면 자동으로 update문이 나갑니다.
+        //변경 후 다시 save를 호출하세요.
         Optional<Product> product = productRepository.findById(id);
         product.ifPresent(p -> {
             p.setName(newName);
@@ -141,4 +137,23 @@ class ProductRepositoryTest {
         assertEquals("짜장면", p.getName());
     }
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
